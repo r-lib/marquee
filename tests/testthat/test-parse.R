549fff3e65c16a661d0b62e6d192eb5a2abd427a
Thomas Lin Pedersen <thomasp85@gmail.com>
2024-05-06 13:36:18 +0200

    skip file snapshots on non-mac

--- tests/testthat/test-parse.R
+++ tests/testthat/test-parse.R
@@ -3,11 +3,13 @@ test_that("parse works", {
   expect_s3_class(parsed, "marquee_parsed")
   expect_true(is_parsed(parsed))
 
-  file <- tempfile()
-  write.csv(parsed[, !names(parsed) %in% c("background", "features", "bullets")], file)
-  expect_snapshot_file(file, "parsed.csv")
-
   parsed_with_html <- marquee_parse(markdown_test, ignore_html = FALSE)
   expect_equal(parsed$text[-17], parsed_with_html$text[-17])
   expect_equal(parsed_with_html$text[17], " lines. <span>WHAT</span>")
+
+  skip_on_os("windows")
+  skip_on_os("linux")
+  file <- tempfile()
+  write.csv(parsed[, !names(parsed) %in% c("background", "features", "bullets")], file)
+  expect_snapshot_file(file, "parsed.csv")
 })
